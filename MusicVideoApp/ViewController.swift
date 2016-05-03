57e11d991402e07cc182fa4515b2cbc3b14a4e8b
Narada Jayasingha <narada@venturit.com>
2016-05-03 19:41:03 +0530

    Completed Part-2

--- MusicVideoApp/ViewController.swift
+++ MusicVideoApp/ViewController.swift
@@ -14,8 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //Call API
+        let api = APIManager()
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=50/json", completion: didLoadData)
     }
+    
+    func didLoadData(result:String) {
+        
+        let alert = UIAlertController(title: (result), message: nil, preferredStyle: .ActionSheet)
+        
+        let okAction = UIAlertAction(title: "ok", style: .Default) {
+            action -> Void in
+        }
+        
+        alert.addAction(okAction)
+        self.presentViewController(alert, animated: true, completion: nil)
 
-
+    }
 }
 
